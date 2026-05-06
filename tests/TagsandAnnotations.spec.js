bf614c0e91acc3299e2f13d74240b139f7e8434c
Zeeshank124 <zeshank1098@gmail.com>
2026-05-06 23:52:55 +0500

    Added tags and annoations exmples

---
+++ tests/TagsandAnnotations.spec.js
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test';
+
+test.skip('Test to skip', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+
+});
+test('Test to fail', async ({page}) => {
+    await page.expect(page).toHaveTitle('Swag Labs123'); // this will fail as the title is not matching
+    test.fail()
+});
+test.fixme('Test to fix', async ({page}) => {
+});
+test('Test to slow', async ({page}) => {
+    test.slow() 
+    await page.goto('https://www.saucedemo.com/');
+});
+// test.only('Test to run only this test', async ({page}) => {
+//     await page.goto('https://www.saucedemo.com/');
+// });
+//use --grep "@smoke" to run only the tests with @smoke tag
+//use --grep-invert "@smoke" to run all the tests except the ones with @smoke tag
+test('Smoke Test @smoke', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+});
+
+
